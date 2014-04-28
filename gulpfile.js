0cf52b3d096cae3f0390aa5460e4dfccfb84d5fa
admangum <adam.mangum@autodesk.com>
2014-04-28 13:48:41 -0700

    scoping bootstrap styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp');
 var plugin = require('gulp-load-plugins')();
 var wiredep = require('wiredep');
-
+gulp.task('styles', function(){
+	return gulp.src('less/bootstrap-scope.less')
+		.pipe(plugin.less())
+		.pipe(gulp.dest('stylesheets'));
+});
 gulp.task('build', function(){
 	return gulp.src('index.template.html')
 		.pipe(wiredep.stream())
