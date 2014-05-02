62e611feedce72bfb8ad28978f656d68a6955684
admangum <adam.mangum@autodesk.com>
2014-05-02 01:38:44 -0700

    renaming test

--- test/filterpanel-test.js
+++ test/filterpanel-test.js
@@ -25,9 +25,10 @@ describe('filter', function(){
       	'displayName': 'Age'
       }]
     };
-    var filter = $('<div/>').filter(filterOptions).data('upcycle-filter');
-	it('adds facet options based on dataset', function(){
-		var facets = filter.option('facets');
+  var filterpanel = $('<div/>').filterpanel(filterOptions).data('upcycle-filterpanel');
+	
+  it('adds facet options based on dataset', function(){
+		var facets = filterpanel.option('facets');
 		expect(facets).to.be.a('array');
 		expect(facets).to.have.length(2);
 		expect(facets[0].options).to.be.a('array');
@@ -36,82 +37,82 @@ describe('filter', function(){
 		expect(facets[1].options).to.have.length(4);
 	});
   it('does not attempt to determine facet options if data source not provided', function(){
-    filter.option({
+    filterpanel.option({
       'data': null
     });
-    var facets = filter.option('facets');
+    var facets = filterpanel.option('facets');
     expect(facets).to.be.a('array');
     expect(facets).to.have.length(2);
     expect(facets[0].options).to.have.length(0);
   });
   it('includes selected facets with change events', function(done){
-    filter.element.one('filterchange', function(event, data){
-      expect(data.selectedFacets.name).to.have.length(1);
+    filterpanel.element.one('filterpanelchange', function(event, data){
+      expect(data.selectedFacets[0].options).to.have.length(1);
       done();
     });
-    var $checkboxes = filter.element.find('[type="checkbox"]');
+    var $checkboxes = filterpanel.element.find('[type="checkbox"]');
     $checkboxes[0].checked = true;
     $checkboxes.eq(0).trigger('change');
   });
   it('includes filtered dataset with change events', function(done){
-    filter.option('data', filterOptions.data);
-    filter.element.one('filterchange', function(event, data){
+    filterpanel.option('data', filterOptions.data);
+    filterpanel.element.one('filterpanelchange', function(event, data){
       expect(data.filteredData).not.to.be.undefined;
       expect(data.filteredData).to.have.length(3);
       done();
     });
-    var $checkboxes = filter.element.find('[type="checkbox"]');
+    var $checkboxes = filterpanel.element.find('[type="checkbox"]');
     $checkboxes[0].checked = true;
     $checkboxes[1].checked = true;
     $checkboxes.eq(0).trigger('change');
   });
 
   it('#set toggles checkboxes', function(done){
-    filter
+    filterpanel
       .clear()
       .option({'dataExtraction': null})
-      .element.one('filterchange', function(event, data){
-        expect(data.selectedFacets.name).to.have.length(1);
-        expect(data.selectedFacets.name[0]).to.equal('Mike');
+      .element.one('filterpanelchange', function(event, data){
+        expect(data.selectedFacets[0].options).to.have.length(1);
+        expect(data.selectedFacets[0].options[0]).to.equal('Mike');
         done();
       });
     // omitting arg after facets to select means, toggle those checkboxes
-    filter.set({
+    filterpanel.set({
       'name': 'Mike'
     });
   });
   
   it('#set sets textboxes explicitly', function(done){
-    filter
+    filterpanel
       .clear()
       .option({'dataExtraction': null})
-      .element.one('filterchange', function(event, data){
-        expect(data.selectedFacets.name).to.be.undefined;
+      .element.one('filterpanelchange', function(event, data){
+        expect(data.selectedFacets).to.have.length(0);
         done();
       });
     // passing false means deselect the checkbox
-    filter.set({
+    filterpanel.set({
       'name': 'Mike'
     }, false);
   });
   it('#set sets multiple textboxes explicitly', function(done){
-    filter
+    filterpanel
       .clear()
       .option({'dataExtraction': null})
-      .element.one('filterchange', function(event, data){
-        expect(data.selectedFacets.name).to.be.length(2);
-        expect(data.selectedFacets.name[1]).to.equal('Aaron');
+      .element.one('filterpanelchange', function(event, data){
+        expect(data.selectedFacets[0].options).to.be.length(2);
+        expect(data.selectedFacets[0].options[1]).to.equal('Aaron');
         done();
       });
     // passing true means select the checkbox
-    filter.set({
+    filterpanel.set({
       'name': ['Mike', 'Aaron']
     }, true);
   });
 
   it('provides dataExtraction option', function(done){
-    filter.clear();
-    filter.option({
+    filterpanel.clear();
+    filterpanel.option({
       'dataExtraction': function(obj, key){
         return obj.attributes[key];
       },
@@ -124,12 +125,12 @@ describe('filter', function(){
           'name': 'Aaron'
         }
       }]
-    }).element.one('filterchange.x', function(event, data){
-      expect(data.selectedFacets.name).to.be.length(1);
+    }).element.one('filterpanelchange', function(event, data){
+      expect(data.selectedFacets[0].options).to.have.length(1);
       expect(data.filteredData).to.be.length(1);
       done();
     });
-    filter.set({
+    filterpanel.set({
       'name': 'Mike'
     });
   });
