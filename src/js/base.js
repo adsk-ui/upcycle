316ebe879d9c0144f9f3741558ac4671cc5200e7
Adam Yee <adamjyee@gmail.com>
2014-07-23 15:23:27 -0700

    obtain templates via templateNamespace
    Recursively finds the templates in the specified namespace. Throws an
    error if not found.

--- src/js/base.js
+++ src/js/base.js
@@ -13,8 +13,18 @@ $.widget('upcycle.base', {
 		return template(this._getTemplateContext.apply(this, arguments));
 	},
 	'_getTemplate': function(templateName){
+		var templates = (function(){return this;})(),
+			spaces = this.option('templatesNamespace').split('.');
+		for (var i=0; i<spaces.length; i++) {
+			if (_.has(templates, spaces[i])) {
+				templates = templates[ spaces[i] ];
+			}
+			else {
+				throw new Error(this.option('templatesNamespace') + ' not found');
+			}
+		}
 		templateName = templateName || this.options.templateName;
-		return eval(this.option('templatesNamespace'))[templateName || this.option('templateName')];
+		return templates[templateName || this.option('templateName')];
 	},
 	'_getLabel': function(label){
 		return this.options.localizeLabels ?  $.i18n.prop( label ) : label;
