677ab8285303c62f0ccb3906cd883412f3b22153
admangum <adam.mangum@autodesk.com>
2015-07-14 16:29:13 -0700

    Cleans up moreless.test.js

--- test/moreless.test.js
+++ test/moreless.test.js
@@ -14,34 +14,32 @@ describe('moreless', function(){
         '</li>'+
     '</ul>';
     var paragraph = '<div id="moreless-min-height"><p>Kale chips hella salvia fashion axe. Food truck PBR&B Kickstarter hella. Forage shabby chic church-key pickled pour-over. Carles wolf gentrify, fingerstache kale chips small batch disrupt skateboard Helvetica Austin letterpress cray tattooed.</p></div>';
-    var $list, $paragraph;
+
     before(function(){
-    	$paragraph = $(paragraph).appendTo('#sandbox-inner').children('p').moreless({
-        	'linkContainer': '#moreless-min-height',
-            'truncateText': true
+    	this.$paragraph = $(paragraph).appendTo('#sandbox-inner').children('p').moreless({
+        	linkContainer: '#moreless-min-height',
+            truncateText: true
         }).end();
-        $list = $(list).appendTo('#sandbox-inner').moreless({
-        	'minItems': 1
+        this.$list = $(list).appendTo('#sandbox-inner').moreless({
+        	minItems: 1
         });
     });
 
     after(function(){
-      $list.remove();
-      $paragraph.remove();
+      this.$list.remove();
+      this.$paragraph.remove();
     });
 
 	it('has no conflict', function(){
 		chai.expect($.fn.moreless.noConflict).to.be.a('function');
 	});
 	it('hides more items', function(){
-		
-		expect($list.children('li:visible')).to.have.length(1);
-		$list.remove();
-		$list = $(list)
+		expect(this.$list.children('li:visible')).to.have.length(1);
+		this.$list.remove();
+		this.$list = $(list)
 				.appendTo('#sandbox-inner')
 				.moreless({'minItems': 'same-y'});
 
-		expect($list.children('li:visible')).to.have.length(1);
-		
+		expect(this.$list.children('li:visible')).to.have.length(1);
 	});
 });
